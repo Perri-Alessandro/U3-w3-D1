948fa3f3fa41af6626f78f0f3f05093c9f771fcd
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2024-02-13 22:34:31 +0100

    routering to home

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,5 +1,7 @@
 import { useSelector } from "react-redux";
 import Job from "./Job";
+import { Link } from "react-router-dom";
+import { Row } from "react-bootstrap";
 
 const Favorite = () => {
   const favorites = useSelector((state) => state.favorites.content);
@@ -7,6 +9,11 @@ const Favorite = () => {
 
   return (
     <div>
+      <Row className="text-center mt-3">
+        <Link className="col text-center" to="/">
+          HOME
+        </Link>
+      </Row>
       <h2 className="text-center mt-3">Favorites Jobs</h2>
       {favorites.map((jobData) => (
         <Job displayTitle={false} key={jobData._id} data={jobData} />
